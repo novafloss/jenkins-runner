e0aa79ba6369a582c564644c291cc7adf67cbcda
Étienne BERSAC (bersace) <etienne.bersac@people-doc.com>
2016-11-17 18:23:25 +0100

    Version 2.26

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='jenkins-yml',
-    version='2.25',
+    version='2.26',
     entry_points={
         'console_scripts': ['jenkins-yml-runner=jenkins_yml:runner_script'],
         'jenkins_yml.runners': ['unconfined=jenkins_yml.runner:unconfined'],
